aa77dd6f0cef5ae9548a03fe7df88492ea2992be
ankith8 <ankith8@gmail.com>
2018-11-21 21:38:17 +0530

    app completed

---
+++ backend/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const mongoose = require('mongoose');
+const postRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
+const path = require("path");
+
+mongoose.connect('mongodb+srv://ankith:mlqmu2bOOiBQ1ddc@cluster0-scr7v.mongodb.net/node-angular')
+  .then(() => {
+    console.log('Connected to database!!')
+  })
+  .catch(() => {
+    console.log('Connection to database failed');
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/images", express.static(path.join('backend/images')));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type,Accept,Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+
+app.use("/api/posts", postRoutes);
+app.use("/api/user", userRoutes);
+
+module.exports = app;
